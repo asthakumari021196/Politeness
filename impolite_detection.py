b9c57eca3bbfecf1d92901b65e0ef51b40353dea
asthakumari021196 <astha.kumari0211@gmail.com>
2022-07-26 23:25:09 +0200

    adding mlflow code

--- impolite_detection.py
+++ impolite_detection.py
@@ -7,6 +7,10 @@ from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+import mlflow
+import mlflow.sklearn
+from urllib.parse import urlparse
+
 # import tensorflow as tf
 # from tensorflow.keras import regularizers
 
@@ -60,7 +64,10 @@ class PolitenessDetector:
         predictions_NB = Naive.predict(self.Test_X_Tfidf)
         print(predictions_NB)
         # Use accuracy_score function to get the accuracy
-        print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, self.Test_Y)*100)  
+        acc_score = accuracy_score(predictions_NB, self.Test_Y)*100
+        print("Naive Bayes Accuracy Score -> ",acc_score)  
+        mlflow.log_metric("accuracy",acc_score )
+
 
     def SVMModel(self):
         # Classifier - Algorithm - SVM
@@ -73,7 +80,9 @@ class PolitenessDetector:
         print('prediction started')
         predictions_SVM = SVM.predict(self.Test_X_Tfidf)
         # Use accuracy_score function to get the accuracy
-        print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, self.Test_Y)*100)      
+        acc_score = accuracy_score(predictions_SVM, self.Test_Y)*100
+        print("SVM Accuracy Score -> ",acc_score)  
+        mlflow.log_metric("accuracy",acc_score )
 
     def detect(self):
         df = self.data
@@ -83,30 +92,56 @@ class PolitenessDetector:
         Encoder = LabelEncoder()
         Train_Y = Encoder.fit_transform(Train_Y)
         Test_Y = Encoder.fit_transform(Test_Y)
-        sample = ["open the box and put cheese in it","can you close the door","It is appreciated if it could be improved","email this to the tax office","pass me the book"," Thank You for the help"," would you like some coffee ?"]
-        sampley = [0,0,1,0,0,1,1]
+        # sample = ["open the box and put cheese in it","can you close the door","It is appreciated if it could be improved","email this to the tax office","pass me the book"," Thank You for the help"," would you like some coffee ?"]
+        # sampley = [0,0,1,0,0,1,1]
         Tfidf_vect = TfidfVectorizer(max_features=5000)
         Tfidf_vect.fit(df['txt'].astype(str))
         Train_X_Tfidf = Tfidf_vect.transform(Train_X.astype(str))
-        # Test_X_Tfidf = Tfidf_vect.transform(Test_X.astype(str))
-        sample_X_TFidf = Tfidf_vect.transform(sample)
+        Test_X_Tfidf = Tfidf_vect.transform(Test_X.astype(str))
+        # sample_X_TFidf = Tfidf_vect.transform(sample)
 
         self.Train_X_Tfidf = Train_X_Tfidf
-        # self.Test_X_Tfidf = Test_X_Tfidf
-        self.Test_X_Tfidf = sample_X_TFidf
+        self.Test_X_Tfidf = Test_X_Tfidf
+        # self.Test_X_Tfidf = sample_X_TFidf
 
         self.Train_Y = Train_Y
-        # self.Test_Y = Test_Y
-        self.Test_Y = sampley
+        self.Test_Y = Test_Y
+        # self.Test_Y = sampley
 
         print("test",Train_X_Tfidf.shape)
 
         # print("test",Test_X_Tfidf.shape)
-        
-        print("model run")
-        self.SVMModel()
-        self.NaiveBayesModel()
-        # self.LSTM()
+                # (1) update the data in the running real time
+        mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+        # (2) this is model registry
+        registery_uri = 'sqlite:///mlflow.db'
+
+        # (3) update the date in the real time from the above sqlitedb
+        mlflow.tracking.set_tracking_uri(registery_uri)
+        # Running the MLflow -> SVM Model and Naive Bayes
+        with mlflow.start_run():
+            print("model run")
+            model1 = self.SVMModel()
+            model2 =self.NaiveBayesModel()
+            # self.LSTM()
+            
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            # Model registry does not work with file store
+            if tracking_url_type_store != "file":
+
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                mlflow.sklearn.log_model(model1, "model_1", registered_model_name="SVM")
+                mlflow.sklearn.log_model(model2, "model_2", registered_model_name="NaiveBayes")
+
+            else:
+                mlflow.sklearn.log_model(model1, "model1")    
+                mlflow.sklearn.log_model(model2, "model2")    
+
 
 
 
