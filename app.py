e8f904009ee92e0ba3bd4f42e130d13483625854
asthakumari021196 <astha.kumari0211@gmail.com>
2022-09-18 18:33:57 +0200

    adding frontend framework

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import  naive_bayes, svm
 import numpy as np
+import mlflow
+import mlflow.pyfunc
+
 import data_polarity
 
 # Declare a Flask app
@@ -15,8 +18,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    sent = ""
-    polarity = ""
+
     # If a form is submitted
     if request.method == "POST":
         # Unpickle classifier        
@@ -26,9 +28,22 @@ def main():
         sent = []
         sent.append(str(text))
         len_of_text = len(sent[0].split())
+
+        mlflow.set_tracking_uri("http://127.0.0.1:5000")
+        registery_uri = 'sqlite:///mlflow.db'
+        mlflow.tracking.set_tracking_uri(registery_uri)
+
+        # mlflow.set_experiment('case-study-one')
+        model_svm  = mlflow.pyfunc.load_model(model_uri=f"models:/SVM/Staging")
+        model_naive  = mlflow.pyfunc.load_model(model_uri=f"models:/NaiveBayes/Staging")
+
+
         if len_of_text < 3:
-            sent = 'Enter another sentence, sentence is too short'
-            
+            err = 'Enter another sentence, sentence is too short'
+            polarity = ""
+            sent_naive = ""
+            sent_svm = ""
+     
 
         else:
             dp = data_polarity.Polarity()
@@ -38,21 +53,30 @@ def main():
 
             Test_X_Tfidf = Tfidf_vect.transform(sent)
             print("text_x_tfidf ", Test_X_Tfidf)
-            SVM  = pickle.load(open('svm.pkl', 'rb'))
-            print("model ", SVM)
-            print("Model loaded")
+
 
             # Get prediction
-            prediction = SVM.predict(Test_X_Tfidf)
-            if prediction == 0:
-                sent = "impolite"
+            prediction_svm = model_svm.predict(Test_X_Tfidf)
+            prediction_naive = model_naive.predict(Test_X_Tfidf)
+
+            err = ""
+            if prediction_svm == 0:
+                sent_svm = "impolite"
             else:
-                sent = "polite"    
+                sent_svm = "polite"    
+
+            if prediction_naive == 0:
+                sent_naive = "impolite"
+            else:
+                sent_naive = "polite"                 
                 
     else:
-        sent = ""
+        sent_naive = ""
+        sent_svm = ""
+        polarity = ""
+        err = ""
         
-    return render_template("website.html", sent = sent, polarity = polarity)
+    return render_template("website.html", sent_svm = sent_svm, polarity = polarity, sent_naive = sent_naive, err = err)
 # Running the app
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0',port = 80,debug = True)
--- impolite_detection.py
+++ impolite_detection.py
@@ -1,14 +1,19 @@
-from cgi import test
 import pandas as pd
 import time
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import mlflow
 import mlflow.sklearn
+import pickle
+
 from urllib.parse import urlparse
 
 # import tensorflow as tf
@@ -19,55 +24,36 @@ class PolitenessDetector:
     def __init__(self,filename) -> None:
         dataRead = pd.read_csv(filename)
         df = pd.DataFrame(dataRead)
-        print(df.shape)
-        self.data = df.head(50000)
-
-    # def LSTM(self):
-    #     max_features =50000
-    #     embedding_dim =16
-    #     sequence_length = 200
-    #     train_ds = tf.data.Dataset.from_tensor_slices((self.Train_X_Tfidf,self.Train_Y))
-    #     test_ds = tf.data.Dataset.from_tensor_slices((self.Test_X_Tfidf,self.Test_Y))
-    #     model = tf.keras.Sequential()
-    #     model.add(tf.keras.layers.Embedding(max_features +1, embedding_dim, input_length=sequence_length,\
-    #                                         embeddings_regularizer = regularizers.l2(0.005))) 
-    #     model.add(tf.keras.layers.Dropout(0.4))
-
-    #     model.add(tf.keras.layers.LSTM(embedding_dim,dropout=0.2, recurrent_dropout=0.2,return_sequences=True,\
-    #                                                                 kernel_regularizer=regularizers.l2(0.005),\
-    #                                                                 bias_regularizer=regularizers.l2(0.005)))
-
-    #     model.add(tf.keras.layers.Flatten())
-
-    #     model.add(tf.keras.layers.Dense(512, activation='relu',\
-    #                                     kernel_regularizer=regularizers.l2(0.001),\
-    #                                     bias_regularizer=regularizers.l2(0.001),)) 
-
-        
-
-    #     model.summary()
-    #     model.compile(loss=tf.keras.losses.BinaryCrossentropy(),optimizer=tf.keras.optimizers.Adam(1e-3),metrics=[tf.keras.metrics.BinaryAccuracy()])                                
-    #     epochs = 3
-    # # Fit the model using the train and test datasets.
-    # #history = model.fit(x_train, train_labels,validation_data= (x_test,test_labels),epochs=epochs )
-    #     model.fit(train_ds.batch(1024),
-    #                             epochs= epochs ,
-    #                         test=test_ds.batch(1024),
-    #                 verbose=1)
-
-
+        self.data = df.head(200000)
 
     def NaiveBayesModel(self):
         Naive = naive_bayes.MultinomialNB()
         Naive.fit(self.Train_X_Tfidf,self.Train_Y)
+        pickle.dump(Naive, open('naive.pkl', 'wb'))
+        mlflow.log_param("test data shape", self.Test_X_Tfidf.shape[0])
+
+
         # predict the labels on validation dataset
         predictions_NB = Naive.predict(self.Test_X_Tfidf)
-        print(predictions_NB)
+        
         # Use accuracy_score function to get the accuracy
         acc_score = accuracy_score(predictions_NB, self.Test_Y)*100
-        print("Naive Bayes Accuracy Score -> ",acc_score)  
-        mlflow.log_metric("accuracy",acc_score )
+        preci_score = precision_score(predictions_NB, self.Test_Y)
+        recall = recall_score(predictions_NB, self.Test_Y)
+        f1 = f1_score(predictions_NB, self.Test_Y)
 
+        mlflow.log_metric("accuracy_naive",acc_score )
+        mlflow.log_metric("precision_naive",preci_score )
+        mlflow.log_metric("recall_naive",recall )
+        mlflow.log_metric("f1_naive",f1 )
+
+
+        print("Naive Bayes Accuracy Score -> ",acc_score)  
+        print("Naive Bayes Precision Score -> ",preci_score)  
+        print("Naive Bayes Recall Score -> ",recall)  
+        print("Naive Bayes F1 Score -> ",f1)  
+
+        return Naive
 
     def SVMModel(self):
         # Classifier - Algorithm - SVM
@@ -76,26 +62,46 @@ class PolitenessDetector:
         print('model fitting')
 
         SVM.fit(self.Train_X_Tfidf,self.Train_Y)
+        pickle.dump(SVM, open('svm.pkl', 'wb'))
+
+        mlflow.log_param("test data shape", self.Test_X_Tfidf.shape[0])
         # predict the labels on validation dataset
         print('prediction started')
+
+
         predictions_SVM = SVM.predict(self.Test_X_Tfidf)
+        
         # Use accuracy_score function to get the accuracy
         acc_score = accuracy_score(predictions_SVM, self.Test_Y)*100
+        preci_score = precision_score(predictions_SVM, self.Test_Y)
+        recall = recall_score(predictions_SVM, self.Test_Y)
+        f1 = f1_score(predictions_SVM, self.Test_Y)
+
+        mlflow.log_metric("accuracy_svm",acc_score )
+        mlflow.log_metric("precision_svm",preci_score )
+        mlflow.log_metric("recall_svm",recall )
+        mlflow.log_metric("f1_svm",f1 )
+
         print("SVM Accuracy Score -> ",acc_score)  
-        mlflow.log_metric("accuracy",acc_score )
+        print("SVM Precision Score -> ",preci_score)  
+        print("SVM Recall Score -> ",recall)  
+        print("SVM F1 Score -> ",f1)  
+
+        return SVM
+        
 
     def detect(self):
         df = self.data
-        print(df.head())
         Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(df['txt'],df['polite'],test_size=0.3)
-        print("train test split")
         Encoder = LabelEncoder()
         Train_Y = Encoder.fit_transform(Train_Y)
         Test_Y = Encoder.fit_transform(Test_Y)
-        # sample = ["open the box and put cheese in it","can you close the door","It is appreciated if it could be improved","email this to the tax office","pass me the book"," Thank You for the help"," would you like some coffee ?"]
+        # sample = ["open the box and put cheese in it", "can you close the door", "It is appreciated if it could be improved","email this to the tax office","pass me the book"," Thank You for the help"," would you like some coffee ?"]
         # sampley = [0,0,1,0,0,1,1]
         Tfidf_vect = TfidfVectorizer(max_features=5000)
         Tfidf_vect.fit(df['txt'].astype(str))
+        pickle.dump(Tfidf_vect, open("vectorizer.pickle", "wb"))
+
         Train_X_Tfidf = Tfidf_vect.transform(Train_X.astype(str))
         Test_X_Tfidf = Tfidf_vect.transform(Test_X.astype(str))
         # sample_X_TFidf = Tfidf_vect.transform(sample)
@@ -119,13 +125,14 @@ class PolitenessDetector:
 
         # (3) update the date in the real time from the above sqlitedb
         mlflow.tracking.set_tracking_uri(registery_uri)
+        
         # Running the MLflow -> SVM Model and Naive Bayes
         with mlflow.start_run():
             print("model run")
             model1 = self.SVMModel()
             model2 =self.NaiveBayesModel()
-            # self.LSTM()
-            
+
+
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
             # Model registry does not work with file store
